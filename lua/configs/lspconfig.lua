56741b5a8c62112ea218c4836d62fe683f1eb631
Bilal Haroon <bilal.a.haroon@protonmail.com>
2026-07-20 10:06:46 -0400

    syntax cleanup

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,20 +1,25 @@
 require("nvchad.configs.lspconfig").defaults()
 
 print("hello from lspconfig")
-local lspconfig = require "lspconfig"
+-- local lspconfig = require "lspconfig"
 local servers = { "html", "cssls", "lua_ls", "pyright", "gopls"} -- Add your LSP here
 
-local nvlsp = require "nvchad.configs.lspconfig"
 
-for _, lsp in ipairs(servers) do
-  vim.lsp.config(lsp, {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  })
-  
-  -- Enable the configured LSP
-  vim.lsp.enable(lsp)
-end
+vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- to configure lsps further read :h vim.lsp.config
+
+-- local nvlsp = require "nvchad.configs.lspconfig"
+--
+-- for _, lsp in ipairs(servers) do
+--   vim.lsp.config(lsp, {
+--     on_attach = nvlsp.on_attach,
+--     on_init = nvlsp.on_init,
+--     capabilities = nvlsp.capabilities,
+--   })
+--
+--   -- Enable the configured LSP
+--   vim.lsp.enable(lsp)
+-- end
+--
+
